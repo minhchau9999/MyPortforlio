156f8672b34bd8f748d24c3060384e5e0360133d
Minh Chau <minh.chau@gmail.com>
2023-06-14 16:57:38 +0700

    add images, static files

--- personal/settings.py
+++ personal/settings.py
@@ -1,5 +1,6 @@
 
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "personal\static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
